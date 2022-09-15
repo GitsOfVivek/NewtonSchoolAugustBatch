26c0870b5aaca60ac7aeb93dfd2278f3a0b66303
Vivek Kumar <95522287+GitsOfVivek@users.noreply.github.com>
2022-09-15 10:35:12 +0530

    Create Queen's attack.java

---
+++ If else ladder, Switch case, Ternary Operator - In Class/Queen's attack.java
@@ -0,0 +1,14 @@
+static int QueenAttack(int X, int Y, int P, int Q){
+//Enter your code here
+    if(X == P) {
+        return 1;
+    }  
+    if(Y == Q) {
+        return 1;
+    }
+    if(Math.abs(X - P) == Math.abs(Y - Q)) {
+        return 1;
+    }else {
+        return 0;
+    }
+}
