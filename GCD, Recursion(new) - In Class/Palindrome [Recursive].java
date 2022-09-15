fa15638cb6b9c6fdfb09262fca240387e047b801
Vivek Kumar <95522287+GitsOfVivek@users.noreply.github.com>
2022-09-15 10:59:57 +0530

    Create Palindrome [Recursive].java

---
+++ GCD, Recursion(new) - In Class/Palindrome [Recursive].java
@@ -0,0 +1,10 @@
+static boolean check_Palindrome(String str,int s, int e) 
+    {
+        if(s >= e) {
+            return true;
+        }
+        if(str.charAt(s) != str.charAt(e)) {
+            return false;
+        }
+            return check_Palindrome(str,s+1,e-1);
+    }
