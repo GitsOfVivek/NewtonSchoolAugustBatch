bc2fff21811474cb5dab1a1318a3163dd711fc62
Vivek Kumar <95522287+GitsOfVivek@users.noreply.github.com>
2022-11-23 19:02:34 +0530

    Create Insert node at the head.java

---
+++ Single Linked List, Double Linked List, Circular Linked LIst - In Class/Insert node at the head.java
@@ -0,0 +1,19 @@
+/*
+class Node {
+    Node next;
+    int val;
+
+    Node(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+*/
+
+public static Node addElement(Node head, int k) {
+//enter your code here
+    Node num = new Node(k);
+    num.next = head;
+    head = num;
+    return head;
+}
